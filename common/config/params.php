5d36b3209bcf954523673a5b5c893a4648b08b9d
Erice Yang <ericeyany@gmail.com>
2015-04-12 17:08:25 +0800

    test ./gitignore

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'admin@ericeyang.com',
+    'supportEmail' => 'support@ericeyang.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
